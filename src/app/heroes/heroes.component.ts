5932a9587111ac5041d2cdd904e3f662500613a5
LE GARREC Vincent <github@le-garrec.fr>
2023-12-25 22:48:14 +0100

    Chapter 3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
 
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
@@ -8,7 +9,7 @@ import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, NgFor, NgIf, UpperCasePipe],
+  imports: [FormsModule, NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
