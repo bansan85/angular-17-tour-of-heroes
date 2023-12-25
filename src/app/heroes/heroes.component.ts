7b3d9c61605bed2b07f77c9c345dd88e529e1502
LE GARREC Vincent <github@le-garrec.fr>
2023-12-25 22:25:27 +0100

    Chapter 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { Hero } from '../hero';
-import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [FormsModule, NgFor, NgIf, UpperCasePipe],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
